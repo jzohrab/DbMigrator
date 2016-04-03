636b47bd5cddb1c0ebe71169cdad4c32754c219a
Jeff Zohrab <jzohrab@gmail.com>
2016-04-03 17:33:44 -0400

    Remove comment cruft only.

--- examples/mysql/test_mysql.py
+++ examples/mysql/test_mysql.py
@@ -2,18 +2,6 @@ import unittest
 import sys
 import os
 
-# sys.path.append(os.path.abspath(sys.path[0]) + '/../examples/mysql')
-# import examples
-# import examples
-# from examples.mysql import MySqlExample
-# from mysql import MySqlExample
-# import mysql
-
-# import examples
-# import examples.mysql
-# from examples.mysql import mysql  # MySqlExample
-# from .. import examples
-# from examples import mysql
 from .mysql import MySqlExample
 
 class MySqlExample_Tests(unittest.TestCase):
