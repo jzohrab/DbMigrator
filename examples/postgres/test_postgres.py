c750a389de1f9e97d7dcabac0050cc5f25da74d3
Jeff Zohrab <jzohrab@gmail.com>
2016-04-03 17:34:16 -0400

    Refactor import only.

--- examples/postgres/test_postgres.py
+++ examples/postgres/test_postgres.py
@@ -2,14 +2,12 @@ import unittest
 import sys
 import os
 
-sys.path.append(os.path.abspath(sys.path[0]) + '/../examples/postgres')
-
-import postgres
+from .postgres import PostgresExample
 
 class PostgresExample_Tests(unittest.TestCase):
 
     def test_sanity_check(self):
-        d = postgres.PostgresExample.build_driver()
+        d = PostgresExample.build_driver()
         d.is_debug_printing = False
         d.main(['example.py', '-nsu'])
 
