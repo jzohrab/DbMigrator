ceafdda207b891bad33e15d0c8f7a678cd6d2327
Jeff Zohrab <jeff.zohrab@rakops.com>
2016-04-03 17:31:31 -0400

    Clean up test output.

--- examples/postgres/test_postgres.py
+++ examples/postgres/test_postgres.py
@@ -10,6 +10,7 @@ class PostgresExample_Tests(unittest.TestCase):
 
     def test_sanity_check(self):
         d = postgres.PostgresExample.build_driver()
+        d.is_debug_printing = False
         d.main(['example.py', '-nsu'])
 
 
