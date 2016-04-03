1254e6da889788c319541da68231b0e77a16748e
Jeff Zohrab <jzohrab@gmail.com>
2016-04-03 17:31:31 -0400

    Clean up test output.

--- examples/mysql/test_mysql.py
+++ examples/mysql/test_mysql.py
@@ -20,6 +20,7 @@ class MySqlExample_Tests(unittest.TestCase):
 
     def test_sanity_check(self):
         d = MySqlExample.build_driver()
+        d.is_debug_printing = False
         d.main(['example.py', '-nsu'])
 
 
