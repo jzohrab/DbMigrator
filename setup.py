d854c7be7fd8181ff28f0a958453b0e95cca3311
Jeff Zohrab <jzohrab@gmail.com>
2016-04-09 23:54:04 -0400

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='DbMigrator',
+      maintainer='Jeff Zohrab',
+      maintainer_email='jzohrab@gmail.com',
+      version='0.1',
+      url='https://github.com/jzohrab/DbMigrator',
+      license='MIT',
+      packages=['DbMigrator'])
